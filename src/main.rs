ac8fca4219caf385ac791bc69e862dc2fa3d9995
JYICova <shaleelcova@live.com>
2026-01-09 20:52:14 +0000

    Shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    let x = 5;
+    println!("The value of x is: {x}");
+
+    let x = x + 1;
+    {
+        let x = x + 5;
+        println!("The value of x is: {x}");
+    }
+    println!("The value of x is: {x}");
 }
+
