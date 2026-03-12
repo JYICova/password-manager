c25cbe91fc43b79d1241d254bc827aea15237ba7
JYICova <shaleelcova@live.com>
2026-03-12 14:14:58 +0000

    Latest Update

--- src/auth.rs
+++ src/auth.rs
@@ -1,9 +1,8 @@
 use argon2::{
-    password_hash::{PasswordHash, PasswordVerifier},
-    Argon2
+    Argon2, PasswordHasher, password_hash::{SaltString}
 };
 
-use rand_core::OsRng;
+use rand::rngs::OsRng;
 
 pub fn hash_password(password: &[u8]) -> Result<String, argon2::password_hash::Error> {
     // Generate a cryptographically secure salt
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
+mod auth;
 
 fn main() {
     let password: &[u8; 8] = b"hunter42";
     // let salt: SaltString = SaltString::generate(&mut OsRng);
 
+    let hash: Result<String, argon2::password_hash::Error> = auth::hash_password(password);
+
     println!("{:?}", password);
-    // println!("{}", salt);
-
-    println!("Hello, world!");
-    let x = 5;
-    println!("The value of x is: {x}");
-
-    let x = x + 1;
-    {
-        let x = x + 5;
-        println!("The value of x is: {x}");
-    }
-    println!("The value of x is: {x}");
+    println!("{:?}", hash);
 }
 
