ce46443d80d13e33a08dac0914242d110460804a
JYICova <shaleelcova@live.com>
2026-06-08 20:41:29 +0100

    refactor: made API more user friendly

--- src/auth.rs
+++ src/auth.rs
@@ -1,10 +1,11 @@
 use argon2::{
     Argon2, PasswordHasher, password_hash::{SaltString}
 };
+use argon2::password_hash::Error;
 
 use rand::rngs::OsRng;
 
-pub fn hash_password(password: &[u8]) -> Result<String, argon2::password_hash::Error> {
+pub fn hash_password(password: &str) -> Result<String, Error> {
     // Generate a cryptographically secure salt
     let salt: SaltString = SaltString::generate(&mut OsRng);
 
@@ -12,8 +13,8 @@ pub fn hash_password(password: &[u8]) -> Result<String, argon2::password_hash::E
     let argon2 = Argon2::default();
 
     // Hash password
-    let password_hash = argon2.hash_password(password, &salt)?;
-
+    let password_hash = argon2.hash_password(password.as_bytes(), &salt)?;
+    println!("password hash: {}", password_hash.to_string());
     // Return PHC formatted hash string
     Ok(password_hash.to_string())
 }
