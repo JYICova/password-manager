b4535f114eafd7385718b8698b58cf315fbd8763
JYICova <shaleelcova@live.com>
2026-06-08 20:04:05 +0100

    Program Accepts User Input

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 mod auth;
+use std::io;
 
 fn main() {
-    let password: &[u8; 8] = b"hunter42";
+    let user_input: String = get_user_input();
+    println!("Entered Password: {}", user_input);
+
+    let password: &[u8] = user_input.trim().as_bytes();
     // let salt: SaltString = SaltString::generate(&mut OsRng);
 
     let hash: Result<String, argon2::password_hash::Error> = auth::hash_password(password);
 
-    println!("{:?}", password);
-    println!("{:?}", hash);
+    println!("Password Binary: {:?}", password);
+    println!("Hash: {:?}", hash);
 }
+
+fn get_user_input() -> String {
+    let mut user_input = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
+    
+    return user_input
+}
+
 
